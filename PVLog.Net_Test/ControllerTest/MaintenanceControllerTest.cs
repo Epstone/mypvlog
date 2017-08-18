b619f20a3bc80735e017d605d186ece811dd295c
Patrick Epstein <Patrick.Epstein@zuehlke.com>
2017-08-18 16:49:29 +0200

    cleanup

--- PVLog.Net_Test/ControllerTest/MaintenanceControllerTest.cs
+++ PVLog.Net_Test/ControllerTest/MaintenanceControllerTest.cs
@@ -1,73 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NUnit.Framework;
-using PVLog.Controllers;
-using PVLog.DataLayer;
-using Moq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
-using PVLog.Models;
-
-namespace solar_tests.ControllerTest
+﻿namespace solar_tests.ControllerTest
 {
-  [TestFixture]
-  public class MaintenanceControllerTest
-  {
+    using System.Collections.Generic;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+    using Moq;
+    using NUnit.Framework;
+    using PVLog.Controllers;
+    using PVLog.DataLayer;
+    using PVLog.Models;
 
-    MaintenanceController _maintenanceController;
-    Mock<I_MeasureRepository> _measureRepositoryMock;
-    Mock<I_PlantRepository> _plantRepositoryMock;
-
-
-    [SetUp]
-    public void Setup()
+    [TestFixture]
+    public class MaintenanceControllerTest
     {
-      _measureRepositoryMock = new Mock<I_MeasureRepository>();
-      _plantRepositoryMock = new Mock<I_PlantRepository>();
-      _maintenanceController = new MaintenanceController(_measureRepositoryMock.Object, _plantRepositoryMock.Object);
+        [SetUp]
+        public void Setup()
+        {
+            _measureRepositoryMock = new Mock<I_MeasureRepository>();
+            _plantRepositoryMock = new Mock<I_PlantRepository>();
+            _maintenanceController = new MaintenanceController(_measureRepositoryMock.Object, _plantRepositoryMock.Object);
+        }
 
+
+        [TearDown]
+        public void TearDown()
+        {
+            _maintenanceController.Dispose();
+        }
+
+        private MaintenanceController _maintenanceController;
+        private Mock<I_MeasureRepository> _measureRepositoryMock;
+        private Mock<I_PlantRepository> _plantRepositoryMock;
+
+        private IEnumerable<Inverter> GetDummyInverterList()
+        {
+            var dummyList = new List<Inverter>();
+            dummyList.Add(new Inverter
+            {
+                InverterId = 4
+            });
+            return dummyList;
+        }
+
+        [Test]
+        public void UpdateStatisticsTest()
+        {
+            // Setup Controller and Context
+            var requestMock = new Mock<HttpRequestBase>();
+            requestMock.SetupGet(x => x.IsLocal).Returns(true);
+
+            var context = new Mock<HttpContextBase>();
+            context.SetupGet(x => x.Request).Returns(requestMock.Object);
+
+            _maintenanceController.ControllerContext = new ControllerContext(context.Object, new RouteData(), _maintenanceController);
+            _maintenanceController.AuthorizationOverride = true;
+
+            // setup plant repository
+            _plantRepositoryMock.Setup(x => x.GetAllInverters()).Returns(() => GetDummyInverterList());
+
+            _maintenanceController.UpdateStatistics("1234");
+
+            //verify that the minute wise calculation process is started
+            _measureRepositoryMock.Verify(x => x.UpdateTemporaryToMinuteWise(It.IsAny<int>()), Times.Once());
+        }
     }
-
-    [Test]
-    public void UpdateStatisticsTest()
-    {
-      // Setup Controller and Context
-      var requestMock = new Mock<HttpRequestBase>();
-      requestMock.SetupGet(x => x.IsLocal).Returns(true);
-
-      var context = new Mock<HttpContextBase>();
-      context.SetupGet(x => x.Request).Returns(requestMock.Object);
-      
-      _maintenanceController.ControllerContext = new ControllerContext(context.Object, new RouteData(), _maintenanceController);
-        _maintenanceController.AuthorizationOverride = true;
-      
-      // setup plant repository
-      _plantRepositoryMock.Setup(x => x.GetAllInverters()).Returns(() => GetDummyInverterList());
-
-      _maintenanceController.UpdateStatistics("1234");
-
-      //verify that the minute wise calculation process is started
-      _measureRepositoryMock.Verify(x => x.UpdateTemporaryToMinuteWise(It.IsAny<int>()), Times.Once());
-    }
-
-    private IEnumerable<PVLog.Models.Inverter> GetDummyInverterList()
-    {
-      var dummyList = new List<Inverter>();
-      dummyList.Add(new Inverter()
-      {
-        InverterId = 4
-      });
-      return dummyList;
-    }
-
-
-      [TearDown]
-      public void TearDown()
-      {
-          _maintenanceController.Dispose();
-      }
-  }
 }
