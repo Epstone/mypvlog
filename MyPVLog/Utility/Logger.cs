af104b885ee25bd724fe720871eac341c97a516e
Patrick Epstein <Patrick.Epstein@zuehlke.com>
2017-08-19 18:28:25 +0200

    adjusted logger

--- MyPVLog/Utility/Logger.cs
+++ MyPVLog/Utility/Logger.cs
@@ -1,53 +1,48 @@
 ﻿using System;
-using PVLog.DataLayer;
 
 namespace PVLog.Utility
 {
+    using Microsoft.ApplicationInsights;
+
     public static class Logger
     {
-        public static SeverityLevel ApplicationLogLevel { get; set; }
-
+        static TelemetryClient telemetry = new TelemetryClient();
         public static void Log(Exception ex, SeverityLevel lvl, string customMessage)
         {
-            var loggerDb = new UtilDatabase();
-            loggerDb.AddLog(lvl, ex.Message, ex.StackTrace, customMessage);
+            telemetry.TrackTrace(customMessage, MapLevel(lvl));
+            telemetry.TrackException(ex);
 
+        }
+
+        private static Microsoft.ApplicationInsights.DataContracts.SeverityLevel MapLevel(SeverityLevel lvl)
+        {
+            switch (lvl)
+            {
+                case SeverityLevel.Error:
+                    return Microsoft.ApplicationInsights.DataContracts.SeverityLevel.Error;
+                case SeverityLevel.Info:
+                case SeverityLevel.Startup:
+                    return Microsoft.ApplicationInsights.DataContracts.SeverityLevel.Information;
+                case SeverityLevel.Warning:
+                    return Microsoft.ApplicationInsights.DataContracts.SeverityLevel.Warning;
+            }
+
+            return Microsoft.ApplicationInsights.DataContracts.SeverityLevel.Error;
         }
 
         public static void LogError(Exception ex)
         {
-            AddLogToDB(ex.Message, ex.StackTrace, SeverityLevel.Error, string.Empty);
-        }
-
-        private static void AddLogToDB(string exceptionMessage, string stackTrace, SeverityLevel lvl, string customMessage)
-        {
-            if (lvl >= ApplicationLogLevel)
-            {
-                var loggerDb = new UtilDatabase();
-                loggerDb.AddLog(lvl, exceptionMessage, stackTrace, customMessage);
-            }
-
+            telemetry.TrackException(ex);
         }
 
         public static void LogInfo(string msg)
         {
-            AddLogToDB(string.Empty, string.Empty, SeverityLevel.Info, msg);
-        }
-
-        public static SeverityLevel StringToSeverityLevel(string lvl)
-        {
-            SeverityLevel result = (SeverityLevel)Enum.Parse(typeof(SeverityLevel), lvl);
-            return result;
-        }
-
-        internal static void SetApplicationLogLevel(string logLevel)
-        {
-            Logger.ApplicationLogLevel = StringToSeverityLevel(logLevel);
+            telemetry.TrackTrace(msg, Microsoft.ApplicationInsights.DataContracts.SeverityLevel.Information);
         }
 
         public static void LogWarning(string msg)
         {
-            AddLogToDB(string.Empty, string.Empty, SeverityLevel.Warning, msg);
+            telemetry.TrackTrace(msg, Microsoft.ApplicationInsights.DataContracts.SeverityLevel.Warning);
         }
     }
 
