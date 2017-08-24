d91a3190c34f3d77bca507d2b92b34bb7b899e23
Patrick Epstein <Patrick.Epstein@zuehlke.com>
2017-08-24 16:40:15 +0200

    code cleanup

--- PVLog.Net_Test/DatabaseTest/UserNotificationTest.cs
+++ PVLog.Net_Test/DatabaseTest/UserNotificationTest.cs
@@ -12,31 +12,62 @@
     [TestFixture]
     public class UserNotificationTest
     {
+        [SetUp]
+        public void Setup()
+        {
+            plantRepositoryMock = new Mock<I_PlantRepository>();
+            inactivePlantId4Days = 123;
+            UserNotifications.ResetCache();
+        }
+
         private UserNotifications userNotifications;
         private Mock<I_PlantRepository> plantRepositoryMock;
         private int inactivePlantId4Days;
         private SolarPlant solarPlantInactive4Days;
         private SolarPlant solarPlantInactive11Days;
 
-        [SetUpAttribute]
-        public void Setup()
+        private void Given_a_plant_which_is_inactive_for_11_days()
         {
-            plantRepositoryMock = new  Mock<I_PlantRepository>();
-            inactivePlantId4Days = 123;
-            UserNotifications.ResetCache();
+            plantRepositoryMock.Setup(x => x.GetAllPlants()).Returns(() =>
+            {
+                solarPlantInactive11Days = new SolarPlant
+                {
+                    PlantId = 1234,
+                    LastMeasureDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(11))
+                };
+                return new List<SolarPlant>
+                {
+                    solarPlantInactive11Days
+                };
+            });
+            userNotifications = new UserNotifications(plantRepositoryMock.Object);
+        }
+
+        private void Given_a_plant_which_is_inactive_for_4_days()
+        {
+            solarPlantInactive4Days = new SolarPlant
+            {
+                PlantId = inactivePlantId4Days,
+                LastMeasureDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(4))
+            };
+
+            plantRepositoryMock.Setup(x => x.GetAllPlants()).Returns(() => new List<SolarPlant>
+            {
+                solarPlantInactive4Days
+            });
+            userNotifications = new UserNotifications(plantRepositoryMock.Object);
         }
 
         [Test]
-        public void Given_a_plant_with_no_recent_activity_in_last_3_days_Then_a_notification_is_returned()
+        public void Given_a_plant_which_is_first_3_days_off_And_then_11_days_off_The_correct_notificationJobsAreCreated()
         {
             Given_a_plant_which_is_inactive_for_4_days();
-
-            this.userNotifications = new UserNotifications(plantRepositoryMock.Object);
-            var plantNotifications = userNotifications.GetPlantNotifications();
-
-            PlantNotification actualNotification = plantNotifications.First();
-            actualNotification.plant.ShouldBeEquivalentTo(solarPlantInactive4Days);
-            actualNotification.NotificationType.Should().Be(NotificationType.Inactivity3Days);
+            var plantNotification = userNotifications.GetPlantNotifications().First();
+            plantNotification.NotificationType.Should().Be(NotificationType.Inactivity3Days);
+            solarPlantInactive4Days.LastMeasureDate = DateTime.UtcNow.Date.Subtract(TimeSpan.FromDays(11));
+            plantNotification = userNotifications.GetPlantNotifications().First();
+            plantNotification.NotificationType.Should().Be(NotificationType.Inactivity10days);
+            plantNotification.Done.Should().BeFalse();
         }
 
         [Test]
@@ -52,6 +83,19 @@
         }
 
         [Test]
+        public void Given_a_plant_with_no_recent_activity_in_last_3_days_Then_a_notification_is_returned()
+        {
+            Given_a_plant_which_is_inactive_for_4_days();
+
+            userNotifications = new UserNotifications(plantRepositoryMock.Object);
+            var plantNotifications = userNotifications.GetPlantNotifications();
+
+            PlantNotification actualNotification = plantNotifications.First();
+            actualNotification.plant.ShouldBeEquivalentTo(solarPlantInactive4Days);
+            actualNotification.NotificationType.Should().Be(NotificationType.Inactivity3Days);
+        }
+
+        [Test]
         public void Given_the_get_notification_jobs_call_is_done_twice_Then_an_old_job_is_marked_as_done()
         {
             Given_a_plant_which_is_inactive_for_11_days();
@@ -59,51 +103,6 @@
             plantNotifications.First().Done.Should().BeFalse();
             plantNotifications = userNotifications.GetPlantNotifications();
             plantNotifications.First().Done.Should().BeTrue();
-        }
-
-        [Test]
-        public void Given_a_plant_which_is_first_3_days_off_And_then_11_days_off_The_correct_notificationJobsAreCreated()
-        {
-            Given_a_plant_which_is_inactive_for_4_days();
-            var plantNotification = userNotifications.GetPlantNotifications().First();
-            plantNotification.NotificationType.Should().Be(NotificationType.Inactivity3Days);
-            solarPlantInactive4Days.LastMeasureDate = DateTime.UtcNow.Date.Subtract(TimeSpan.FromDays(11));
-            plantNotification = userNotifications.GetPlantNotifications().First();
-            plantNotification.NotificationType.Should().Be(NotificationType.Inactivity10days);
-            plantNotification.Done.Should().BeFalse();
-        }
-
-        private void Given_a_plant_which_is_inactive_for_11_days()
-        {
-            this.plantRepositoryMock.Setup(x => x.GetAllPlants()).Returns(() =>
-            {
-                solarPlantInactive11Days = new SolarPlant()
-                {
-                    PlantId = 1234,
-                    LastMeasureDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(11)),
-                };
-                return new List<SolarPlant>()
-                {
-                    solarPlantInactive11Days,
-                };
-            });
-            this.userNotifications = new UserNotifications(plantRepositoryMock.Object);
-
-        }
-
-        private void Given_a_plant_which_is_inactive_for_4_days()
-        {
-            solarPlantInactive4Days = new SolarPlant()
-            {
-                PlantId = inactivePlantId4Days,
-                LastMeasureDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(4)),
-            };
-
-            this.plantRepositoryMock.Setup(x => x.GetAllPlants()).Returns(() => new List<SolarPlant>()
-            {
-                solarPlantInactive4Days,
-            });
-            this.userNotifications = new UserNotifications(plantRepositoryMock.Object);
         }
     }
 }
