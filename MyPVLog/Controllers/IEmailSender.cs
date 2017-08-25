763f766965a1c6a193f34a0e67f75413f47c5594
Patrick Epstein <Patrick.Epstein@zuehlke.com>
2017-08-25 14:37:02 +0200

    added email smpt client usage

--- MyPVLog/Controllers/IEmailSender.cs
+++ MyPVLog/Controllers/IEmailSender.cs
@@ -1,7 +1,20 @@
 ﻿namespace PVLog.Controllers
 {
+    using System.Net.Mail;
+
     public interface IEmailSender
     {
         void Send(string body, string subject, string recipientAddress);
+    }
+
+    internal class EmailSender : IEmailSender
+    {
+        public void Send(string body, string subject, string recipientAddress)
+        {
+            new SmtpClient("smtp.server.com", 25).Send("info@mypvlog.de",
+                recipientAddress,
+                subject,
+                body);
+        }
     }
 }
