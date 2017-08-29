8e859925490d8ffc42331f830a7a7bacfe32aa13
Patrick Epstein <Patrick.Epstein@zuehlke.com>
2017-08-29 19:45:51 +0200

    fix email settings checkbox

--- MyPVLog/Controllers/PlantController.cs
+++ MyPVLog/Controllers/PlantController.cs
@@ -16,209 +16,207 @@ using PoliteCaptcha;
 
 namespace PVLog.Controllers
 {
-  [HandleError]
-  public class PlantController : MyController
-  {
-
-    public PlantController()
+    [HandleError]
+    public class PlantController : MyController
     {
 
+        public PlantController()
+        {
+
+        }
+
+        public PlantController(I_PlantRepository plantRepository)
+        {
+            _plantRepository = plantRepository;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet, Authorize]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+
+        [Authorize, HttpPost, ValidateSpamPrevention]
+        public ActionResult Add(SolarPlant model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                model.Password = Utils.GenerateRandomString(8);
+                var plantId = _plantRepository.CreatePlant(model);
+                _plantRepository.StoreUserPlantRelation(CurrentUserId, plantId, E_PlantRole.Owner);
+
+                return RedirectToAction("View", new { id = plantId, name = model.Name });
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult View(int id)
+        {
+            var plant = _plantRepository.GetPlantById(id);
+            var kwhRepository = new KwhRepository();
+
+            // build view model
+            var plantModel = PlantHomeModel.Create(plant);
+            //var today =Utils.GetTodaysDate();
+            var today = DateTimeUtils.GetTodaysDate();
+            plantModel.SummaryTable = new PlantSummaryTableModel();
+
+            plantModel.SummaryTable.Today = _dataProvider.GetkwhAndMoneyPerTimeFrame(today, today.AddDays(1), id, E_TimeMode.day);
+            plantModel.SummaryTable.ThisMonth = _dataProvider.GetkwhAndMoneyPerTimeFrame(DateTimeUtils.FirstDayOfMonth(), DateTimeUtils.FirstDayNextMonth(), id, E_TimeMode.month);
+            plantModel.SummaryTable.ThisYear = _dataProvider.GetkwhAndMoneyPerTimeFrame(DateTimeUtils.FirstDayOfYear(), DateTimeUtils.FirstDayNextYear(), id, E_TimeMode.year);
+
+
+            //is user allowed to edit the plant?
+            plantModel.HeaderModel = GetHeaderModel(plant);
+
+            return View(plantModel);
+        }
+
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            if (!IsAuthorizedForPlant(id))
+                ThrowNotAuthorizedForPlantException();
+
+            var model = _plantRepository.GetPlantById(id);
+            return View("EditPlant", model);
+
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult Edit(SolarPlant model)
+        {
+            var plantToUpdate = _plantRepository.GetPlantById(model.PlantId);
+
+            if (!IsAuthorizedForPlant(model.PlantId))
+                ThrowNotAuthorizedForPlantException();
+
+            if (ModelState.IsValid)
+            {
+                // store all updated fields
+                plantToUpdate.Name = model.Name;
+                plantToUpdate.AutoCreateInverter = model.AutoCreateInverter;
+                plantToUpdate.PeakWattage = model.PeakWattage;
+                plantToUpdate.PostalCode = model.PostalCode;
+                plantToUpdate.EmailNotificationsEnabled = model.EmailNotificationsEnabled;
+
+                _plantRepository.UpdatePlant(plantToUpdate);
+
+                ViewData["Message"] = "Ihre Änderungen wurden übernommen.";
+            }
+            return View("EditPlant", plantToUpdate);
+        }
+
+        [HttpGet]
+        public ActionResult List()
+        {
+            var model = new PlantListModel()
+            {
+                Plants = _plantRepository.GetAllPlants()
+            };
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Month(int id)
+        {
+            //get kwh data
+            string googleTableContent = _dataProvider.GoogleDataTableContent(DateTimeUtils.FirstDayOfMonth(),
+                                                                  DateTimeUtils.FirstDayNextMonth(), id,
+                                                                  E_EurKwh.kwh, E_TimeMode.day);
+            var plant = _plantRepository.GetPlantById(id);
+            var model = new PlantDayModel()
+            {
+                Plant = plant,
+                GoogleData = googleTableContent,
+                HeaderModel = GetHeaderModel(plant)
+            };
+
+            return View(model);
+        }
+
+        private PlantHeader GetHeaderModel(SolarPlant plant)
+        {
+            return new PlantHeader()
+            {
+                IsEditingAllowed = IsAuthorizedForPlant(plant.PlantId),
+                Plant = plant
+            };
+        }
+
+        [HttpGet]
+        public ActionResult Year(int id)
+        {
+            //get kwh data
+            string googleTableContent = _dataProvider.GoogleDataTableContent(DateTimeUtils.FirstDayOfYear(),
+                                                                  DateTimeUtils.FirstDayNextYear(), id,
+                                                                  E_EurKwh.kwh, E_TimeMode.month);
+            var plant = _plantRepository.GetPlantById(id);
+            var model = new PlantDayModel()
+            {
+                Plant = plant,
+                GoogleData = googleTableContent,
+                HeaderModel = GetHeaderModel(plant)
+            };
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Decade(int id)
+        {
+            //get kwh data
+            string googleTableContent = _dataProvider.GoogleDataTableContent(new DateTime(2010, 1, 1),
+                                                                   new DateTime(2020, 1, 1), id,
+                                                                  E_EurKwh.kwh, E_TimeMode.year);
+            var plant = _plantRepository.GetPlantById(id);
+            var model = new PlantDayModel()
+            {
+                Plant = plant,
+                GoogleData = googleTableContent,
+                HeaderModel = GetHeaderModel(plant)
+            };
+
+            return View(model);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult AddInverter(int id)
+        {
+            if (!IsAuthorizedForPlant(id))
+                ThrowNotAuthorizedForPlantException();
+
+            //add new inverter to plant
+            _plantRepository.CreateInverter(id, null, 0.1F, "Neuer Generator");
+
+            return RedirectToAction("Edit", new { id });
+        }
+
+        private void ThrowNotAuthorizedForPlantException()
+        {
+            throw new HttpException(401, "You are not allowed to edit this plant");
+        }
+
+        private bool IsAuthorizedForPlant(int id)
+        {
+            return (Request.IsAuthenticated &&
+                   (_plantRepository.IsOwnerOfPlant(CurrentUserId, id) || IsAdmin));
+        }
     }
-
-    public PlantController(I_PlantRepository plantRepository)
-    {
-      _plantRepository = plantRepository;
-    }
-
-    public ActionResult Index()
-    {
-      return View();
-    }
-
-    [HttpGet, Authorize]
-    public ActionResult Add()
-    {
-      return View();
-    }
-
-
-    [Authorize, HttpPost, ValidateSpamPrevention]
-    public ActionResult Add(SolarPlant model)
-    {
-
-      if (ModelState.IsValid)
-      {
-        model.Password = Utils.GenerateRandomString( 8 );
-        var plantId = _plantRepository.CreatePlant( model );
-        _plantRepository.StoreUserPlantRelation( CurrentUserId, plantId, E_PlantRole.Owner );
-
-        return RedirectToAction( "View", new { id = plantId, name = model.Name } );
-      }
-      else
-      {
-        return View( model );
-      }
-    }
-
-    [HttpGet]
-    public ActionResult View(int id)
-    {
-      var plant = _plantRepository.GetPlantById( id );
-      var kwhRepository = new KwhRepository();
-
-      // build view model
-      var plantModel = PlantHomeModel.Create( plant );
-      //var today =Utils.GetTodaysDate();
-      var today = DateTimeUtils.GetTodaysDate();
-      plantModel.SummaryTable = new PlantSummaryTableModel();
-
-      plantModel.SummaryTable.Today = _dataProvider.GetkwhAndMoneyPerTimeFrame( today, today.AddDays( 1 ), id, E_TimeMode.day );
-      plantModel.SummaryTable.ThisMonth = _dataProvider.GetkwhAndMoneyPerTimeFrame( DateTimeUtils.FirstDayOfMonth(), DateTimeUtils.FirstDayNextMonth(), id, E_TimeMode.month );
-      plantModel.SummaryTable.ThisYear = _dataProvider.GetkwhAndMoneyPerTimeFrame( DateTimeUtils.FirstDayOfYear(), DateTimeUtils.FirstDayNextYear(), id, E_TimeMode.year );
-
-
-      //is user allowed to edit the plant?
-      plantModel.HeaderModel = GetHeaderModel( plant );
-
-      return View( plantModel );
-    }
-
-
-    [HttpGet]
-    [Authorize]
-    public ActionResult Edit(int id)
-    {
-      if (!IsAuthorizedForPlant( id ))
-        ThrowNotAuthorizedForPlantException();
-
-      var model = _plantRepository.GetPlantById( id );
-      return View( "EditPlant", model );
-
-    }
-
-    [HttpPost]
-    [Authorize]
-    public ActionResult Edit(SolarPlant model)
-    {
-      var plantToUpdate = _plantRepository.GetPlantById( model.PlantId );
-
-      if (!IsAuthorizedForPlant( model.PlantId ))
-        ThrowNotAuthorizedForPlantException();
-
-      if (ModelState.IsValid)
-      {
-        // store all updated fields
-        plantToUpdate.Name = model.Name;
-        plantToUpdate.AutoCreateInverter = model.AutoCreateInverter;
-        plantToUpdate.PeakWattage = model.PeakWattage;
-        plantToUpdate.PostalCode = model.PostalCode;
-
-        _plantRepository.UpdatePlant( plantToUpdate );
-
-
-        ViewData["Message"] = "Ihre Änderungen wurden übernommen.";
-      }
-      return View( "EditPlant", plantToUpdate );
-
-
-    }
-
-    [HttpGet]
-    public ActionResult List()
-    {
-      var model = new PlantListModel()
-      {
-        Plants = _plantRepository.GetAllPlants()
-      };
-
-      return View( model );
-    }
-
-    [HttpGet]
-    public ActionResult Month(int id)
-    {
-      //get kwh data
-      string googleTableContent = _dataProvider.GoogleDataTableContent( DateTimeUtils.FirstDayOfMonth(),
-                                                            DateTimeUtils.FirstDayNextMonth(), id,
-                                                            E_EurKwh.kwh, E_TimeMode.day );
-      var plant = _plantRepository.GetPlantById( id );
-      var model = new PlantDayModel()
-      {
-        Plant = plant,
-        GoogleData = googleTableContent,
-        HeaderModel = GetHeaderModel( plant )
-      };
-
-      return View( model );
-    }
-
-    private PlantHeader GetHeaderModel(SolarPlant plant)
-    {
-      return new PlantHeader()
-      {
-        IsEditingAllowed = IsAuthorizedForPlant( plant.PlantId ),
-        Plant = plant
-      };
-    }
-
-    [HttpGet]
-    public ActionResult Year(int id)
-    {
-      //get kwh data
-      string googleTableContent = _dataProvider.GoogleDataTableContent( DateTimeUtils.FirstDayOfYear(),
-                                                            DateTimeUtils.FirstDayNextYear(), id,
-                                                            E_EurKwh.kwh, E_TimeMode.month );
-      var plant = _plantRepository.GetPlantById( id );
-      var model = new PlantDayModel()
-      {
-        Plant = plant,
-        GoogleData = googleTableContent,
-        HeaderModel = GetHeaderModel( plant )
-      };
-
-      return View( model );
-    }
-
-    [HttpGet]
-    public ActionResult Decade(int id)
-    {
-      //get kwh data
-      string googleTableContent = _dataProvider.GoogleDataTableContent( new DateTime( 2010, 1, 1 ),
-                                                             new DateTime( 2020, 1, 1 ), id,
-                                                            E_EurKwh.kwh, E_TimeMode.year );
-      var plant = _plantRepository.GetPlantById( id );
-      var model = new PlantDayModel()
-      {
-        Plant = plant,
-        GoogleData = googleTableContent,
-        HeaderModel = GetHeaderModel( plant )
-      };
-
-      return View( model );
-    }
-
-    [Authorize]
-    [HttpGet]
-    public ActionResult AddInverter(int id)
-    {
-      if (!IsAuthorizedForPlant( id ))
-        ThrowNotAuthorizedForPlantException();
-
-      //add new inverter to plant
-      _plantRepository.CreateInverter( id, null, 0.1F, "Neuer Generator" );
-
-      return RedirectToAction( "Edit", new { id } );
-    }
-
-    private void ThrowNotAuthorizedForPlantException()
-    {
-      throw new HttpException( 401, "You are not allowed to edit this plant" );
-    }
-
-    private bool IsAuthorizedForPlant(int id)
-    {
-      return (Request.IsAuthenticated &&
-             (_plantRepository.IsOwnerOfPlant( CurrentUserId, id ) || IsAdmin));
-    }
-  }
 }
